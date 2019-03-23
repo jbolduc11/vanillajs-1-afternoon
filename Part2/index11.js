8dea76c556ca62e99e5c5d35e2de61e5921891c7
Jared <jaredbolduc@yahoo.com>
2019-03-23 15:46:08 -0600

    finished

--- Part2/index11.js
+++ Part2/index11.js
@@ -1,1 +1,17 @@
-console.log("Hello Ashlee")
+console.log("the house always wins")
+
+let id = document.getElementById("idInput");
+let color = document.getElementById("colorInput");
+
+console.log(id);
+console.log(color);
+
+let setCard = () => {
+    const card = document.getElementById(id.value)
+    console.log(card)
+    card.style.color = color.value;
+}
+
+function resetForm() {
+    document.getElementById("cardForm").resetForm
+}
